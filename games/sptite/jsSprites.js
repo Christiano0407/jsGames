bbabbcf0551bb2bf25bf27039d72d405e8e4ef15
Christiano0407 <placebo0704@gmail.com>
2021-11-19 12:04:03 -0600

    frameX and Y / Animation

--- games/sptite/jsSprites.js
+++ games/sptite/jsSprites.js
@@ -13,16 +13,32 @@ const CANVAS_HEIGHT = (canvas.height = 600);
 // ==> Llevar la imagen (sprite) al proyecto <
 const playerImg = new Image();
 playerImg.src = `/games/sptite/img/shadow_dog.png`;
-let x = 100;
-let y = 0;
+//let x = 100;
+//let y = 0;
+const spriteWidth = 575;
+const spriteHeight = 523;
+//> Empezar de principio ( position) <
+let frameX = 0; // 0 * spriteWidth
+let frameY = 0; // 0 * spriteHeight
 
 //> Crear animation <
 function animate() {
   // especificar en donde rect
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(x, 50, 100, 100);
-  //> img
-  ctx.drawImage(playerImg, 0, 0);
+  //ctx.fillRect(x, 50, 100, 100);
+  //> img / Tomar el sprite (encuadrar)
+  //ctx.drawImage(Image, sx, sy, sw, sh, dx, dy, dw, dh);
+  ctx.drawImage(
+    playerImg,
+    frameX * spriteWidth, // Img => recorrido Ancho / Horizontal
+    frameY * spriteHeight, // Recorrido => recorrido Alto / Vertical
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    CANVAS_WIDTH,
+    CANVAS_HEIGHT
+  );
   //> peticion
   requestAnimationFrame(animate);
 }
