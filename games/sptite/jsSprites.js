001d88c1697b50a6d7b94aaf622acb281a679421
Christiano0407 <placebo0704@gmail.com>
2021-11-19 10:25:57 -0600

    sprites Plus

--- games/sptite/jsSprites.js
+++ games/sptite/jsSprites.js
@@ -10,4 +10,22 @@ console.log(ctx);
 const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
 
+// ==> Llevar la imagen (sprite) al proyecto <
+const playerImg = new Image();
+playerImg.src = `/games/sptite/img/shadow_dog.png`;
+let x = 100;
+let y = 0;
+
+//> Crear animation <
+function animate() {
+  // especificar en donde rect
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillRect(x, 50, 100, 100);
+  //> img
+  ctx.drawImage(playerImg, 0, 0);
+  //> peticion
+  requestAnimationFrame(animate);
+}
+animate();
+
 console.groupEnd();
