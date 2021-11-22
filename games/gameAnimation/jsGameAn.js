98e7e73a4b2f2a2324d35856fe835de138b9b802
Christiano0407 <placebo0704@gmail.com>
2021-11-22 15:54:18 -0600

    X-Animation

--- games/gameAnimation/jsGameAn.js
+++ games/gameAnimation/jsGameAn.js
@@ -5,8 +5,13 @@ const canvas = document.getElementById(`canvas1`);
 const ctx = canvas.getContext(`2d`);
 const canvas_width = (canvas.width = 500);
 const canvas_height = (canvas.height = 1000);
-const numberOfEnemies = 50;
+const numberOfEnemies = 25;
 const enemiesArray = [];
+
+//> Traer IMG ==>
+const enemyImage = new Image();
+enemyImage.src = `/games/gameAnimation/img/enemy1.png`;
+let gameFrame = 0;
 
 //(A)>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 // > Representación del Objeto <
@@ -24,17 +29,37 @@ class Enemy {
   constructor() {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.width = 50;
-    this.height = 50;
     this.speed = Math.random() * 4 - 2; // Rango entr 4 u -2;
+    this.spriteWidth = 293; //px
+    this.spriteHeight = 155; //px
+    this.width = this.spriteWidth / 2.5; // > Nuevo Tamaño <
+    this.height = this.spriteHeight / 2.5;
+    this.frame = 0; // Movimiento del sprite (desde cero) <
+    this.flapSpeed = Math.random() * 3 + 1;
   }
   // Methods
   update() {
     this.x += this.speed;
     this.y += this.speed;
+    // Animate sprites >
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
   }
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+    //img => con sus movimientos(sprites)
+    ctx.drawImage(
+      enemyImage,
+      0,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 //> Crear nuevo ==>
@@ -42,7 +67,7 @@ class Enemy {
 for (let i = 0; i < numberOfEnemies; i++) {
   enemiesArray.push(new Enemy());
 }
-console.log(enemiesArray);
+//console.log(enemiesArray);
 //> Animation position <
 function animate() {
   //eliminar cuadros anteriores>
@@ -55,6 +80,7 @@ function animate() {
     enemy.update();
     enemy.draw();
   });
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
